b163d6a7e8acd0d3f3c42c0f9376eecf0fb5ebb4
DolphinWing <dolphinwing74+github@gmail.com>
2022-02-11 18:39:28 +0800

    game hotfix 494138

--- workshop-1993780385/modinfo.lua
+++ workshop-1993780385/modinfo.lua
@@ -5,7 +5,7 @@ version = "1.2.2"
 -- mod author
 author = "DolphinWing and anetrlf2"
 -- mod description in the game
-description = "版本: "..version.." (v493743)\n\n"..
+description = "版本: "..version.." (v494138)\n\n"..
     "吃下「翻譯蒟蒻」之後，會將原本遊戲內的文字換成 MOD 自帶的翻譯本。"..
 	"翻譯本主要以 Klei 簡體中文文檔為主，輔以 Klei 正體中文文檔，"..
 	"並參考 Don't Starve 中文維基，以及一些私心小修正。\n\n"..
