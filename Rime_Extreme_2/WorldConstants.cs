6c9e6406b2acace384959c060d967646605f0c44
DolphinWing <dolphinwing74+github@gmail.com>
2025-01-30 23:52:34 +0800

    update 0K icon and translations

--- Rime_Extreme_2/WorldConstants.cs
+++ Rime_Extreme_2/WorldConstants.cs
@@ -2,28 +2,28 @@
 {
     class WorldConstants
     {
-        public static LocString CLUSTER_NAME = (LocString)"Extreme Rime Classic";
+        public static LocString CLUSTER_NAME = (LocString)"Not 0K Classic";
         public static LocString CLUSTER_DESCRIPTION = (LocString)"0K challenge";
 
-        public static LocString WORLD_NAME = (LocString)"Extreme Rime planet";
+        public static LocString WORLD_NAME = (LocString)"Not 0K planet";
         public static LocString WORLD_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
 
-        public static LocString CLUSTER_SO_NAME = (LocString)"Extreme Rime Cluster";
+        public static LocString CLUSTER_SO_NAME = (LocString)"Not 0K Cluster";
         public static LocString CLUSTER_SO_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
-        public static LocString WORLD_SO_NAME = (LocString)"Extreme Rime asteroid";
+        public static LocString WORLD_SO_NAME = (LocString)"Not 0K asteroid";
         public static LocString WORLD_SO_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
 
-        public static LocString CLUSTER_SO_MINI_NAME = (LocString)"Extreme Rime Mini Cluster";
+        public static LocString CLUSTER_SO_MINI_NAME = (LocString)"Not 0K Mini Cluster";
         public static LocString CLUSTER_SO_MINI_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
-        public static LocString WORLD_SO_MINI_NAME = (LocString)"Extreme Rime asteroid";
+        public static LocString WORLD_SO_MINI_NAME = (LocString)"Not 0K asteroid";
         public static LocString WORLD_SO_MINI_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
 
-        public static LocString CLUSTER_SO_MOONLET_NAME = (LocString)"Extreme Rime Moonlet Cluster";
+        public static LocString CLUSTER_SO_MOONLET_NAME = (LocString)"Not 0K Moonlet Cluster";
         public static LocString CLUSTER_SO_MOONLET_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
-        public static LocString WORLD_SO_MOONLET_NAME = (LocString)"Extreme Rime Moonlet";
+        public static LocString WORLD_SO_MOONLET_NAME = (LocString)"Not 0K Moonlet";
         public static LocString WORLD_SO_MOONLET_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
     }
 }
