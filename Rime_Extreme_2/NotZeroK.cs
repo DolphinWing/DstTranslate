c174efa4ef778563655ea6df1aa8a8a69fadded5
DolphinWing <dolphinwing74+github@gmail.com>
2025-01-31 19:02:47 +0800

    override outer asteroids temperature

--- Rime_Extreme_2/NotZeroK.cs
+++ Rime_Extreme_2/NotZeroK.cs
@@ -1,10 +1,13 @@
 ﻿using HarmonyLib;
+using Klei.CustomSettings;
 using PeterHan.PLib.Core;
 using PeterHan.PLib.Database;
 using ProcGen;
 using ProcGenGame;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace NotZeroK
 {
@@ -22,8 +25,8 @@ namespace NotZeroK
         /// <summary>
         /// The enum value used for 100K subworlds.
         /// </summary>
-        public const Temperature.Range TG_AbsoluteZero = (Temperature.Range)18;
-        public const Temperature.Range TG_SuperCold = (Temperature.Range)19;
+        public const Temperature.Range TG_AbsoluteZero = (Temperature.Range)30;
+        public const Temperature.Range TG_SuperCold = (Temperature.Range)29;
 
         public override void OnLoad(Harmony harmony)
         {
@@ -51,26 +54,50 @@ namespace NotZeroK
                 !temperatureReverseTable.TryGetValue(value, out __result);
         }
 
-        ///// <summary>
-        ///// Applied to TerrainCell to "fix" the temperature range of Volcanoes, Magma Channels,
-        ///// Buried Oil, Subsurface Ocean, and Irregular Oil to 100 K.
-        ///// </summary>
-        //[HarmonyPatch(typeof(TerrainCell), "GetTemperatureRange", typeof(WorldGen))]
-        //public static class TerrainCell_GetTemperatureRange_Patch
-        //{
-        //    /// <summary>
-        //    /// Applied after GetTemperatureRange runs.
-        //    /// </summary>
-        //    internal static void Postfix(WorldGen worldGen, ref Temperature.Range __result)
-        //    {
-        //        var world = worldGen.Settings?.world;
-        //        var temp = __result;
-        //        if (world != null && world.name.StartsWith("NotZeroK.WorldConstants.") && 
-        //            temp > Temperature.Range.VeryCold && temp <= Temperature.Range.ExtremelyHot)
-        //        {
-        //            __result = TG_AbsoluteZero; // Override temp
-        //        }
-        //    }
-        //}
+        // refs: https://github.com/peterhaneve/ONIMods/blob/main/Challenge100K/Challenge100K.cs
+        /// <summary>
+        /// Applied to TerrainCell to "fix" the temperature range of everything.
+        /// </summary>
+        [HarmonyPatch(typeof(TerrainCell), "GetTemperatureRange", typeof(WorldGen))]
+        public static class TerrainCell_GetTemperatureRange_Patch
+        {
+            private static bool IsAbzModded(string prefix)
+            {
+                if (prefix.StartsWith("ABZ-TG")) return true;
+                if (prefix.StartsWith("M-ABZ-TG")) return true;
+                return false;
+            }
+
+            /// <summary>
+            /// Applied after GetTemperatureRange runs.
+            /// </summary>
+            internal static void Postfix(WorldGen worldGen, ref Temperature.Range __result)
+            {
+                if (worldGen.Settings == null) return; // no need to check it
+
+                SettingLevel current = CustomGameSettings.Instance.GetCurrentQualitySetting((SettingConfig)CustomGameSettingConfigs.ClusterLayout);
+                if (current == null) return; // unknown cluster
+
+                ClusterLayout clusterData = SettingsCache.clusterLayouts.GetClusterData(current.id);
+                //Debug.Log("Not0K " + clusterData.GetCoordinatePrefix());
+                string prefix = clusterData.GetCoordinatePrefix();
+                if (IsAbzModded(prefix) == false) return; // we only cares about ABZ
+
+                var world = worldGen.Settings?.world;
+                if (world == null) return; // unknown world
+
+                var temp = __result;
+                if (temp > Temperature.Range.ExtremelyCold && temp <= Temperature.Range.ExtremelyHot)
+                {
+                    if (worldGen.isStartingWorld && temp == Temperature.Range.Mild) {
+                        // ignore it because we don't want dupes die too quickly
+                    } else
+                    {
+                        Debug.Log("Not0K override " + temp);
+                        __result = TG_AbsoluteZero; // Override temp
+                    }
+                }
+            }
+        }
     }
 }
--- Rime_Extreme_2/WorldConstants.cs
+++ Rime_Extreme_2/WorldConstants.cs
@@ -6,24 +6,27 @@
         public static LocString CLUSTER_DESCRIPTION = (LocString)"0K challenge";
 
         public static LocString WORLD_NAME = (LocString)"Not 0K planet";
-        public static LocString WORLD_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
+        public static LocString WORLD_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool Place.\n\n<smallcaps>A little cozy place to start your 0K challenge.</smallcaps>";
 
         public static LocString CLUSTER_SO_NAME = (LocString)"Not 0K Cluster";
         public static LocString CLUSTER_SO_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
         public static LocString WORLD_SO_NAME = (LocString)"Not 0K asteroid";
-        public static LocString WORLD_SO_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
+        public static LocString WORLD_SO_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool Place.\n\n<smallcaps>A little cozy place to start your 0K challenge.</smallcaps>";
+
+        public static LocString WORLD_SO_WARP_NAME = (LocString)"Iced Swamp Asteroid";
+        public static LocString WORLD_SO_WARP_DESCRIPTION = (LocString)"A large-ish, polluted swamp world.\n\n<smallcaps>Iced Swamp Asteroids are full of swampy and marshy areas, but also contain a variety of other biomes diverse enough to keep a determined colony supplied with essentials. Probably.</smallcaps>";
 
         public static LocString CLUSTER_SO_MINI_NAME = (LocString)"Not 0K Mini Cluster";
         public static LocString CLUSTER_SO_MINI_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
         public static LocString WORLD_SO_MINI_NAME = (LocString)"Not 0K asteroid";
-        public static LocString WORLD_SO_MINI_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
+        public static LocString WORLD_SO_MINI_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool Place.\n\n<smallcaps>A little cozy place to start your 0K challenge.</smallcaps>";
 
         public static LocString CLUSTER_SO_MOONLET_NAME = (LocString)"Not 0K Moonlet Cluster";
         public static LocString CLUSTER_SO_MOONLET_DESCRIPTION = (LocString)"0K challenge in Spaced Out!";
 
         public static LocString WORLD_SO_MOONLET_NAME = (LocString)"Not 0K Moonlet";
-        public static LocString WORLD_SO_MOONLET_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool";
+        public static LocString WORLD_SO_MOONLET_DESCRIPTION = (LocString)"Not 0K, But Pretty Cool Place.\n\n<smallcaps>A little cozy place to start your 0K challenge.</smallcaps>";
     }
 }
